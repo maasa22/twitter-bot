8090ed6b41550fb6a5847911854526ecc2a2605e
正木博明 <masaki@maasa22noMacBook-Air.local>
2017-03-20 14:18:53 +0900

    天気速報ほぼ完成

--- my-twitter-bot.py
+++ my-twitter-bot.py
@@ -63,10 +63,10 @@ def job1():
       print("| 天気=", data["weather"][0]["description"])
       print("| 最低気温=", k2c(data["main"]["temp_min"]))
       print("| 最高気温=", k2c(data["main"]["temp_max"]))
-      print("| 気温=", k2c(data["main"]["temp"]))
+      print("| 気温=", (format(k2c(data["main"]["temp"]), '.1f')) )
       print("| 湿度=", data["main"]["humidity"])
       print("| 気圧=", data["main"]["pressure"])
-      #print("| 風向き=", data["wind"]["deg"], " (north:0, east:90, south:180, west:270)")
+      print("| 風向き=", data["wind"]["deg"], " (north:0, east:90, south:180, west:270)")
       print("| 風速度=", data["wind"]["speed"])
       print("")
       #ツイートのみ
@@ -74,8 +74,20 @@ def job1():
       #status = "tokyo : " + str(temp) + "℃"  #投稿するツイート
       present_time =d.strftime("%Y-%m-%d %H:%M:%S")
       #今の時刻を呟かせるようにすると、重複しない
-      status = present_time + '\n' + "気温= " + str(format(k2c(data["main"]["temp"]), '.1f'))
+      #status = present_time + '\n' + "気温= " + str(format(k2c(data["main"]["temp"]), '.1f'))
+
+      status = present_time + '\n' + "都市= " + str(data["name"]) + '\n' \
+            +"天気= "+str(data["weather"][0]["description"]) + '\n' \
+            +"気温= "+ str(format(k2c(data["main"]["temp"]), '.1f')) + "℃" +'\n' \
+            +"湿度= "+ str(data["main"]["humidity"]) +'\n' \
+            +"気圧= "+ str(data["main"]["pressure"]) +'\n' \
+            +"風向き= "+ str(data["wind"]["deg"])+ '\n' \
+            +"風速度= "+ str(data["wind"]["speed"]) + '\n' \
+            +"最低気温= "+ str(k2c(data["main"]["temp_min"])) + "℃" +'\n' \
+            +"最高気温= "+ str(k2c(data["main"]["temp_max"])) + "℃" +'\n' \
+
       t.statuses.update(status=status)
+
 
 
 def job2():
